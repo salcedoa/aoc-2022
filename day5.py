e5fbececf223caf27d2b03f1a04118fead3dc3f5
Andreas Salcedo <71774912+salcedoa@users.noreply.github.com>
2022-12-05 19:17:08 +0000

    Finished Day 5

--- day5.py
+++ day5.py
@@ -1,4 +1,4 @@
-f = open("test.txt","r")
+f = open("day5.txt","r")
 
 # part 1
 from collections import deque
@@ -10,23 +10,58 @@ instructions = inputSections[1]
 
 numOfStacks = int(crateMap.split()[-1])
 
-stacks = {c:deque() for c in range(numOfStacks)}
+# populate initial stacks
+def initializeStacks():
+  stacks = {c:deque() for c in range(numOfStacks)}
+  lines = crateMap.splitlines()
 
-def getInstructionsList(ins):
-  return list(map(lambda x: int(x),re.split("move | from | to ",ins)))
+  x = 0
+  y = 3
+  for s in range(numOfStacks):
+    for c in range(numOfStacks):
+      crate = lines[c][x:y]
+      if bool(re.match("\S",crate)):
+        stacks[s].appendleft(crate)
+    x += 4
+    y += 4
+  return stacks
 
-line = crateMap.splitlines()
-print(line[0][0:3])
-print(line[0][4:7])
-print(bool(re.match("\s",line[0][0:3])))
+stacks = initializeStacks()
 
-x = 0
-y = 3
+# perform instructions
+for i in instructions.splitlines():
+  nums = re.split("move | to | from ",i)
+  for i in range(int(nums[1])):
+    stacks[int(nums[3])-1].append(stacks[int(nums[2])-1].pop())
 
-for i in range(numOfStacks):
-  crate = line[i][x:y]
-  if bool(re.match("\S",crate)):
-    stacks[i].appendleft(crate)
-    
-  x += 4
-  y += 4
+# get last letters
+letters = []
+for s in range(numOfStacks):
+  if stacks[s]: letters.append(stacks[s].pop())
+
+letters = "".join(letters)
+print("".join([x for x in letters if x.isalnum()]))
+
+
+# part 2
+stacks = initializeStacks()
+
+# perform instructions
+for i in instructions.splitlines():
+  nums = re.split("move | to | from ",i)
+  if nums[1] == 1:
+    stacks[int(nums[3])-1].append(stacks[int(nums[2])-1].pop())
+  else:
+    temp = deque()
+    for i in range(int(nums[1])):
+      temp.append(stacks[int(nums[2])-1].pop())
+    for i in range(int(nums[1])):
+      stacks[int(nums[3])-1].append(temp.pop())
+
+# get last letters
+letters = []
+for s in range(numOfStacks):
+  if stacks[s]: letters.append(stacks[s].pop())
+
+letters = "".join(letters)
+print("".join([x for x in letters if x.isalnum()]))
